eedc1007699afa43cbfa8bb49be4bcf6517b18c8
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2025-08-11 17:25:06 -0400

    bleh

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 export default function App() {
     return (
-        <div className="w-full h-screen flex justify-center items-center">
-            <h4 className="text-neutral-200">disbored.</h4>
+        <div className="w-full h-screen flex flex-col justify-center items-center">
+            <h1 className="text-neutral-200">beycord</h1>
+            <h1 className="text-neutral-200">april 1st 2021</h1>
         </div>
+
     )
 }
